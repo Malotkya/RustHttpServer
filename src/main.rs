3254d4d1f996067f1bb218788cf2892dbd2c1f28
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-06-17 16:10:08 -0500

    start to Request

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{ prelude::*, BufReader};
+use std::net::TcpListener;
 
-fn handle_stream(mut stream: TcpStream){
-    let buf_reader = BufReader::new(&mut stream);
-    println!("Buffer: {buf_reader:#?}\n");
-
-    let lines = buf_reader.lines();
-    //println!("Lines: {lines:#?}]n");
-
-    let result = lines.map(|result| result.unwrap());
-    //println!("Result: {result:#?}]n");
-        
-    let http_request: Vec<_> = result.take_while(|line| !line.is_empty()).collect();
-    //println!("Request: {http_request:#?}");
-
-    let status_line  = "HTTP/1.1 200 OK";
-    let message = "Hello World";
-    
-    let response = format!("{status_line}'\r\n\r\n{message}");
-    stream.write_all(response.as_bytes()).unwrap();
-
-        
-}
+mod request;
 
 fn server(listner: TcpListener){
     for stream in listner.incoming() {
         match stream {
             Ok(stream) => {
-                handle_stream(stream);
+                let request = request::Request::new(stream).unwrap();
+
+                println!("{}", request.path());
             }
             Err(e) => {
                 panic!("There was an issue with the incoming message:\n{}", e);
---
+++ src/request.rs
@@ -0,0 +1,57 @@
+use std::io::{ prelude::*, BufReader, Error, ErrorKind};
+use std::net::TcpStream;
+use std::collections::HashMap;
+
+pub struct Request {
+    path: String,
+    method: String,
+    headers: HashMap<String, String>
+}
+
+/** Request
+ * 
+ * Currently only handles get requests.
+ */
+impl Request {
+    pub fn new(mut raw_request: TcpStream )->Result<Request, Error>{
+        let buffer: Vec<_> = BufReader::new(&mut raw_request)
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+
+        let status:Vec<_> = (&buffer[0]).split(" ").collect();
+
+        if status.len() < 2{
+            return Err(Error::new(ErrorKind::InvalidData, "Malformed Http Request!"));
+        } else if status[2].trim() == "HTTP/1.1" {
+            println!("{:?}", status[2]);
+            return Err(Error::new(ErrorKind::InvalidData, "Request is not an Http Request!"));
+        }
+
+        let method:String = String::from(status[0]);
+        let path:String = String::from(status[1]);
+        let mut headers:HashMap<String, String> = HashMap::new();
+
+        for index in 1..buffer.len() {
+            let line:Vec<_> = buffer[index].split(":").collect();
+            headers.insert(line[0].trim().to_string(), line[1].trim().to_string());
+        }
+
+        Ok( Self {
+            method, path, headers
+        })
+    }
+
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+
+    pub fn method(&self) -> &str {
+        &self.method
+    }
+
+    pub fn header(&self, key:&str) -> &str {
+        &(&self.headers)[key]
+    }
+}
