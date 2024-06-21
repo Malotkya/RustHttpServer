3b2d71671a032990da65a71942e732f6f59a4305
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-06-21 12:08:39 -0500

    Start to main path structure.

--- src/path.rs
+++ src/path.rs
@@ -1,9 +1,46 @@
 use std::collections::HashMap;
-use std::io::Result;
+use std::io::{Error, ErrorKind};
+use regex::Regex;
 
 mod lexer;
 mod token;
 mod key;
 
-pub type ParamData = Option<HashMap<String, Vec<String>>>;
-pub type PathFunction = Box<dyn FnMut(ParamData)->Result<String>>;
+lazy_static! {
+    static ref ESCAPE_REGEX:Regex = Regex::new(r#"/([.+*?=^!:${}()[\]|/\\])"#).unwrap();
+}
+
+type Encode = fn(String)->String;
+fn no_encode(s:String)->String{
+    s
+}
+fn escape(str:String)->String {
+    ESCAPE_REGEX.replace_all(&str, "\\$1");
+    return str;
+}
+
+pub type PathOptions = token::CompileOptions;
+
+pub struct Path {
+    keys: Vec<key::Key>,
+    regex: Regex
+}
+
+impl Path {
+    pub fn new(path: String, options: PathOptions)->Result<Path, Error>{
+        let data = token::parse(path, token::ParseOptions::from(options))?;
+        let mut keys = Vec::new();
+        match token::token_to_regexp(data, &mut keys, options) {
+            Ok(regex) => Ok(Path{ keys, regex }),
+            Err(e)=>Err(Error::new(ErrorKind::Other, e))
+        }
+    }
+
+    pub fn match(&self, path:&String)->Vec<Option<String>> {
+
+    }
+
+    pub fn keys(&self)->&Vec<key::Key> {
+        &self.keys
+    }
+}
