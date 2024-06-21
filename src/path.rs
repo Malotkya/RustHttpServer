9ae14a9fb7a115ed8d0f882cb163e38f1d504511
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-06-21 16:43:28 -0500

    Fixed missing escape and testing regex string.

--- src/path.rs
+++ src/path.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
 use regex::Regex;
 
@@ -7,7 +6,7 @@ mod token;
 mod key;
 
 lazy_static! {
-    static ref ESCAPE_REGEX:Regex = Regex::new(r#"/([.+*?=^!:${}()[\]|/\\])"#).unwrap();
+    static ref ESCAPE_REGEX:Regex = Regex::new(r"/([.+*?=^!:${}()\[\]|/\\])").unwrap();
 }
 
 type Encode = fn(String)->String;
@@ -28,16 +27,25 @@ pub struct Path {
 
 impl Path {
     pub fn new(path: String, options: PathOptions)->Result<Path, Error>{
-        let data = token::parse(path, token::ParseOptions::from(options))?;
+        let data = token::parse(path, token::ParseOptions::from(&options))?;
         let mut keys = Vec::new();
         match token::token_to_regexp(data, &mut keys, options) {
-            Ok(regex) => Ok(Path{ keys, regex }),
+            Ok(regex) => {
+                println!("Regex: {:?}", regex.as_str());
+                Ok(Path{ keys, regex })
+            },
             Err(e)=>Err(Error::new(ErrorKind::Other, e))
         }
     }
 
-    pub fn match(&self, path:&String)->Vec<Option<String>> {
+    pub fn match_path(&self, path:&str)->Vec<Option<String>> {
+        let mut output = Vec::new();
 
+        for value in self.regex.find_iter(path) {
+            output.push(Some(value.as_str().to_string()));
+        }
+
+        return output;
     }
 
     pub fn keys(&self)->&Vec<key::Key> {
