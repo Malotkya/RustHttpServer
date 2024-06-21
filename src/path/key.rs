c4c0f44a888fa41b63a6c6d03ec2bff63848f59e
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-06-21 12:06:06 -0500

    Added key function.

--- src/path/key.rs
+++ src/path/key.rs
@@ -1,3 +1,10 @@
+/** /path/keys
+ * 
+ * @author Alex Malotky
+ */
+use crate::path::{Encode, escape};
+
+#[derive(Debug, Clone)]
 pub struct Key {
     pub name: String,
     pub prefix: String,
@@ -5,4 +12,38 @@ pub struct Key {
     pub pattern: String,
     pub modifier: String,
     pub separator: Option<String>
+}
+
+pub type KeyToRegex = Box<dyn FnMut(Key)->String>;
+
+///Key To Regex Generator
+pub fn key_to_regexp_gen(stringify:Encode, delimiter:String)->KeyToRegex {
+    let segment_pattern = format!("`[^{}]+?", escape(delimiter));
+    return Box::new(move |key:Key|->String {
+        let prefix = stringify(key.prefix);
+        let suffix = stringify(key.suffix);
+        let seperator = key.separator.unwrap_or(String::new());
+
+        if !key.name.is_empty() {
+            let pattern = if key.pattern.is_empty() {
+                segment_pattern.clone()
+            } else {
+                key.pattern
+            };
+
+            if !seperator.is_empty() {
+                let modifier = if seperator == "*" {
+                    String::from("?")
+                } else {
+                    String::new()
+                };
+                let split = stringify(seperator);
+                return format!("(?:{}((?:{})(?:{}(?:{}))*){}){}", prefix, pattern, split, pattern, suffix, modifier);
+            } else {
+                return format!("(?:{}({}){}){}", prefix, pattern, suffix, key.modifier);
+            }
+        }
+
+        return format!("`(?:{}{}){}", prefix, suffix, key.modifier);
+    })
 }
