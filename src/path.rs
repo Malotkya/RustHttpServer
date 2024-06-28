6b5120fb2cba653cc8c2894b446e933217a723b7
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-06-28 15:54:49 -0500

    Using replacer to fill HashMap with matches.

--- src/path.rs
+++ src/path.rs
@@ -1,12 +1,14 @@
 use std::io::{Error, ErrorKind};
-use regex::Regex;
+use token::token_to_regexp;
+use regex::{Regex, Captures, Replacer};
+use std::collections::HashMap;
 
 mod lexer;
 mod token;
 mod key;
 
 lazy_static! {
-    static ref ESCAPE_REGEX:Regex = Regex::new(r"/([.+*?=^!:${}()\[\]|/\\])").unwrap();
+    static ref ESCAPE_REGEX:Regex = Regex::new(r"([.+*?=^!:${}()\[\]|/\\])").unwrap();
 }
 
 type Encode = fn(String)->String;
@@ -17,6 +19,35 @@ fn escape(str:String)->String {
     ESCAPE_REGEX.replace_all(&str, "\\$1");
     return str;
 }
+
+pub type Matches = HashMap<String, String>;
+
+struct KeyMapper<'a> {
+    keys: Vec<String>,
+    map: &'a mut Matches
+}
+
+impl Replacer for KeyMapper<'_> {
+    fn replace_append(&mut self, caps: &Captures<'_>, _dst: &mut String) {
+        let mut index = 0;
+        while index < self.keys.len() {
+            let value = String::from(&caps[index+1]);
+            self.map.insert(self.keys[index].clone(), value);
+            index+=1;
+        }
+    }
+}
+
+impl KeyMapper<'_> {
+    pub fn new<'a>(keys: &Vec<key::Key>, map: &'a mut Matches)->KeyMapper<'a>{
+        let mut names: Vec<String> = Vec::new();
+        for key in keys {
+            names.push(key.name.clone());
+        }
+        KeyMapper{ keys: names, map}
+    }
+}
+
 
 pub type PathOptions = token::CompileOptions;
 
@@ -29,22 +60,18 @@ impl Path {
     pub fn new(path: String, options: PathOptions)->Result<Path, Error>{
         let data = token::parse(path, token::ParseOptions::from(&options))?;
         let mut keys = Vec::new();
-        match token::token_to_regexp(data, &mut keys, options) {
+        match token_to_regexp(data, &mut keys, options) {
             Ok(regex) => {
-                println!("Regex: {:?}", regex.as_str());
                 Ok(Path{ keys, regex })
             },
             Err(e)=>Err(Error::new(ErrorKind::Other, e))
         }
     }
 
-    pub fn match_path(&self, path:&str)->Vec<Option<String>> {
-        let mut output = Vec::new();
-
-        for value in self.regex.find_iter(path) {
-            output.push(Some(value.as_str().to_string()));
-        }
-
+    pub fn match_path(&self, path:&str)->Matches {
+        let mut output:HashMap<String, String> = HashMap::new();
+        let map = KeyMapper::new(&self.keys, &mut output);
+        self.regex.replace_all(path, map);
         return output;
     }
 
