9ee5affede1cf81182da1e9155488fbd82da294d
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-07-01 13:21:05 -0500

    Using new server structure.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::net::TcpListener;
 
 const DATA: &str= "<form method='POST'><input name='textbox'/><br/><input type='radio' name='button' value='hello world' /><br/><button>Submit</button></form>";
 
@@ -9,40 +8,21 @@ pub mod request;
 pub mod response;
 pub mod http_status;
 pub mod path;
-//pub mod router;
-
-fn server(listner: TcpListener){
-    for connection in listner.incoming() {
-        let stream = connection.unwrap();
-        let clone = stream.try_clone().unwrap();
-
-        let request = request::Request::new(stream).unwrap();
-        let mut response = response::Response::new(clone);
-
-        //Read Post Data
-        /*if request.method() == "POST" {
-            let buffer: Vec<_> = request.body.bytes().map(|byte|byte.unwrap()).collect();
-            let body_string = String::from_utf8(buffer).unwrap();
-            println!("{:?}", body_string);
-        }*/
-
-        response.status(404).unwrap();
-        response.write(DATA.as_bytes()).unwrap();
-    }
-}
+pub mod router;
+pub mod server;
 
 fn main(){
-    let listner = TcpListener::bind("127.0.0.1:5000");
+    let server = server::Server::new(5000);
 
-    match listner {
-        Ok(listner) => {
-            println!("Ready on port 5000!");
-            server(listner);
-        }
-        Err(e) => {
-            panic!("There was a problem starting that application:\n{}", e);
-        }
-    }
-
+    let mut rt = router::Router::new(path::PathOptions::default()).unwrap();
+    rt.add_method(request::RequestMethod::ALL, |_req:&mut request::Request, res:&mut response::Response|{
+        res.status(200);
+        res.set_header(String::from("Content-Type"), String::from("text/plain"));
+        res.write(DATA.as_bytes());
+    });
     
+    server.start(&||{
+        println!("Ready on port 5000!");
+    });
+
 }
