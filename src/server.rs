a956ead40ca775b3e27ea812c6b8a31998035d7a
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-07-01 13:13:59 -0500

    first (unsuccessful) stab at the server.

---
+++ src/server.rs
@@ -0,0 +1,55 @@
+use crate::router::layer::DynamicLayer;
+use crate::request::Request;
+use crate::response::Response;
+use std::net::{TcpListener, TcpStream};
+use std::io::Result;
+use std::thread;
+
+pub struct Server {
+    layers: Vec<DynamicLayer>,
+    port: u32,
+    
+}
+
+impl Server {
+    pub fn new(port:u32)->Server{
+        Self{port, layers: Vec::new()}
+    }
+
+    pub fn add(&mut self, layer:DynamicLayer){
+        self.layers.push(layer);
+    }
+
+    pub fn start(&self, callback:&dyn Fn())->Result<()>{
+        let listner = TcpListener::bind(format!("127.0.0.1:{}", self.port))?;
+        callback();
+
+        for connection in listner.incoming() {
+            let stream = connection?;
+            let layers = self.layers.clone();
+
+            thread::spawn(||{
+                Server::handle(layers, stream);
+            });
+        }
+
+        Ok(())
+    }
+
+    fn handle(layers: &Vec<DynamicLayer>, stream:TcpStream) {
+        let clone = stream.try_clone().unwrap();
+
+        let mut request = Request::new(stream).unwrap();
+        let mut response = Response::new(clone);
+
+        let query = request.query.clone();
+        for layer in layers {
+            if layer._match(&mut request) {
+                layer.handle(&mut request, &mut response);
+                request.query = query.clone();
+            }
+        }
+
+        //404 and other error handling
+    }
+}
