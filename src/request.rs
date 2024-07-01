5b4edf0e5065746793c6de3a28a2b7d294e7140a
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-07-01 11:10:41 -0500

    Added request mothod enum, and param.

--- src/request.rs
+++ src/request.rs
@@ -6,15 +6,72 @@ use std::io::{prelude::*, BufReader, Error, ErrorKind};
 use std::net::TcpStream;
 use std::collections::HashMap;
 
+#[derive(PartialEq)]
+pub enum RequestMethod {
+    GET,
+    POST,
+    HEAD,
+    PUT,
+    DELETE,
+    OPTIONS,
+    PATCH,
+    ALL
+}
+
+impl RequestMethod {
+    pub fn from(str: &str)->Option<RequestMethod> {
+        if str == "GET" {
+            return Some(Self::GET);
+        }
+
+        if str == "POST" {
+            return Some(Self::POST);
+        }
+
+        if str == "HEAD" {
+            return Some(Self::HEAD);
+        }
+
+        if str == "PUT" {
+            return Some(Self::PUT);
+        } 
+
+        if str == "DELETE" {
+            return Some(Self::DELETE);
+        }
+
+        if str == "OPTIONS" {
+            return Some(Self::OPTIONS);
+        }
+
+        if str == "PATCH" {
+            return Some(Self::PATCH);
+        }
+
+        return None;
+
+    }
+
+    pub fn eq(&self, value:&RequestMethod)->bool{
+        if *self == Self::ALL {
+            return true;
+        }
+
+        return *self == *value;
+    }
+}
+
 /// Http Request
 #[allow(dead_code)]
 pub struct Request {
     url: String,
-    method: String,
+    method: RequestMethod,
     headers: HashMap<String, String>,
     path: String,
-    query: HashMap<String, String>,
-    pub body: BufReader<TcpStream>
+    pub query: String,
+    search: HashMap<String, String>,
+    pub body: BufReader<TcpStream>,
+    params: HashMap<String, String>
 }
 
 #[allow(dead_code)]
@@ -49,9 +106,18 @@ impl Request {
         }
 
         //Data from incoming headers.
-        let method:String = String::from(status[0]);
         let url:String = String::from(status[1]);
         let mut headers:HashMap<String, String> = HashMap::new();
+        let method:RequestMethod;
+
+        match RequestMethod::from(status[0]) {
+            Some(value)=> {
+                method = value;
+            }
+            None => {
+                return Err(Error::new(ErrorKind::InvalidData, "Unknown Request Method!"));
+            }
+        }
 
         for index in 1..raw_headers.len() {
             let line:Vec<_> = raw_headers[index].split(":").collect();
@@ -65,17 +131,17 @@ impl Request {
         if url_array.len() > 1 {
             search_string = String::from(url_array[1]);
         }
-        let mut query = HashMap::new();
+        let mut search = HashMap::new();
 
         for string in search_string.split("&"){
             let buffer: Vec<_> = string.split("=").collect();
             if buffer.len() > 1 {
-                query.insert(String::from(buffer[0].trim()), String::from(buffer[1].trim()));
+                search.insert(String::from(buffer[0].trim()), String::from(buffer[1].trim()));
             }
         }
 
         Ok( Self {
-            method, url, headers, path, query, body: buffer
+            method, url, headers, query: path.clone(), path, search, body: buffer, params: HashMap::new()
         })
     }
 
@@ -83,11 +149,19 @@ impl Request {
         &self.path
     }
  
-    pub fn method(&self) -> &str {
+    pub fn method(&self) -> &RequestMethod {
         &self.method
     }
 
     pub fn header(&self, key:&str) -> &str {
         &(&self.headers)[key]
+    }
+
+    pub fn set_param(&mut self, value:HashMap<String, String>){
+        self.params = value;
+    }
+
+    pub fn param(&self) -> &HashMap<String, String> {
+        &self.params
     }
 }
