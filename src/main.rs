348d78ed57ee964f9298fa288519009d72641f41
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-07-03 15:46:02 -0500

    Take advantage of returning result.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
+use std::io::Result;
 
 pub mod request;
 pub mod response;
@@ -13,14 +14,12 @@ const DATA: &str= "<form method='POST'><input name='textbox'/><br/><input type='
 
 
 fn main(){
-    
-
     let mut rt = router::Router::new(path::PathOptions::default()).unwrap();
-    rt.add_method(request::RequestMethod::ALL, |_req:&mut request::Request, res:&mut response::Response|{
-        println!("Hello World!");
-        res.status(200).unwrap();
-        res.set_header(String::from("Content-Type"), String::from("text/html")).unwrap();
-        res.write(DATA.as_bytes()).unwrap();
+    rt.add_method(request::RequestMethod::ALL, |_req:&mut request::Request, res:&mut response::Response|->Result<()>{
+        res.status(200)?;
+        res.set_header(String::from("Content-Type"), String::from("text/html"))?;
+        res.write(DATA.as_bytes())?;
+        Ok(())
     });
 
     unsafe { 
