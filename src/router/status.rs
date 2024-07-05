267ccda248dd897aacc9e7f761f07fa1564a4179
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-07-05 11:27:06 -0500

    Removed HttpError generic type.

--- src/router/status.rs
+++ src/router/status.rs
@@ -1,13 +1,13 @@
 use crate::error::HttpError;
 use std::io::Result;
 
-type Continue = Option<HttpError<String>>;
+type Continue = Option<HttpError>;
 pub type Status = Result<Continue>;
 
 pub fn next()->Status {
     Ok(None)
 }
 
-pub fn error(err: HttpError<String>)->Status{
+pub fn error(err: HttpError)->Status{
     Ok( Some(err) )
 }
