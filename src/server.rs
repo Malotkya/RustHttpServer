86532f1226cd06cd0ea1fdef90b589605669be3a
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-07-05 11:27:45 -0500

    Switched back to single thread in prep for using async/await.

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,10 @@
 use crate::router::layer::DynamicLayer;
-use crate::request::Request;
+use crate::request::{Request, RequestMethod};
 use crate::response::Response;
 use crate::error::{HttpError, HttpErrorKind};
 use std::net::{TcpListener, TcpStream};
 use std::io::Result;
-use std::thread;
+
 
 pub struct Server {
     layers: Vec<DynamicLayer>,
@@ -21,35 +21,36 @@ impl Server {
         self.layers.push(layer);
     }
 
-    pub fn start(&'static self, callback:&dyn Fn())->Result<()>{
+    pub fn start(&self, callback:&dyn Fn())->Result<()>{
         let listner = TcpListener::bind(format!("127.0.0.1:{}", self.port))?;
         callback();
 
         for connection in listner.incoming() {
             let stream = connection?;
-            let layers = &self.layers;
-
-            thread::spawn(||{
-                Server::handle(layers, stream);
-            });
+            self.handle(stream);
         }
 
         Ok(())
     }
 
-    fn handle(layers: &Vec<DynamicLayer>, stream:TcpStream) {
+    fn handle(&self, stream:TcpStream) {
         let clone = stream.try_clone().unwrap();
 
         let mut request = Request::new(stream).unwrap();
         let mut response = Response::new(clone);
-        let mut error: Option<HttpError<String>> = None;
+        let mut error: Option<HttpError> = None;
+
+        if request.method() == RequestMethod::POST {
+            let body = String::from_utf8(request.body().to_vec()).unwrap();
+            println!("{}", body);
+        }
 
         let query = String::from(request.query());
-        for layer in layers {
+        for layer in &self.layers {
             if layer._match(&mut request) {
                 match layer.handle(&mut request, &mut response) {  
                     Err(e) => {
-                        error = Some(HttpError::new(HttpErrorKind::InternalServerError, format!("{}", e)));
+                        error = Some(HttpError::new(HttpErrorKind::InternalServerError, e));
                         break;
                     }
                     Ok(_) => request.set_query(query.clone())
@@ -64,7 +65,9 @@ impl Server {
 
         if error.is_some() {
             let error = error.unwrap();
-
+            println!("{}", error);
+            let _ = response.status(error.code());
+            response.write(error.message().as_bytes()).unwrap();
         }
     }
 }
