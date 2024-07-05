2cbd72a7b81754f2c272de7d2c88d046b9585a9c
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-07-05 08:54:16 -0500

    Using status in router.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #[macro_use]
 extern crate lazy_static;
-use std::io::Result;
+use crate::router::status::{Status, next};
 
 pub mod request;
 pub mod response;
-pub mod http_status;
 pub mod path;
 pub mod router;
 pub mod server;
+pub mod error;
 
 static mut SERVER:server::Server = server::Server::new(5000);
 const DATA: &str= "<form method='POST'><input name='textbox'/><br/><input type='radio' name='button' value='hello world' /><br/><button>Submit</button></form>";
@@ -15,11 +15,11 @@ const DATA: &str= "<form method='POST'><input name='textbox'/><br/><input type='
 
 fn main(){
     let mut rt = router::Router::new(path::PathOptions::default()).unwrap();
-    rt.add_method(request::RequestMethod::ALL, |_req:&mut request::Request, res:&mut response::Response|->Result<()>{
+    rt.add_method(request::RequestMethod::ALL, |_req:&mut request::Request, res:&mut response::Response|->Status{
         res.status(200)?;
         res.set_header(String::from("Content-Type"), String::from("text/html"))?;
         res.write(DATA.as_bytes())?;
-        Ok(())
+        next()
     });
 
     unsafe { 
