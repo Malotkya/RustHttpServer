177b6be6e985833bcce9fd473d81847e14789427
Alex Malotky <ZimTKE@gmail.com>
2025-08-12 16:28:29 -0500

    Renamed to http_macro

--- http_macro/src/lib.rs
+++ http_macro/src/lib.rs
@@ -1,14 +1,17 @@
 use proc_macro::TokenStream;
 
-mod regex;
+mod path;
+mod router;
+
+mod util;
 
 #[proc_macro]
-pub fn path(input:TokenStream) -> TokenStream {
-    let path_string = syn::parse_macro_input!(input as syn::LitStr).value();
-    let (regex_str, keys) = regex::compile(&path_string, false, false);
+pub fn path(data:TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(data as syn::LitStr).value();
+    let (regex_str, keys) = path::compile(&input, false, false);
 
     quote::quote! {
-        Path{
+        http::Path{
             regex: regex::RegexBuilder::new(#regex_str)
                     .case_insensitive(true).build().unwrap(),
             keys: [#(#keys),*]
@@ -16,34 +19,45 @@ pub fn path(input:TokenStream) -> TokenStream {
     }.into()
 }
 
+#[proc_macro_attribute]
+pub fn router(attrs: TokenStream, input: TokenStream) -> TokenStream {
+    
+    let args: router::RouterAttributes = syn::parse_macro_input!(attrs);
+    let handler: syn::ItemFn = syn::parse_macro_input!(input);
+
+    router::build_router(
+        args, handler
+    ).unwrap().into()
+}
+
 #[cfg(test)]
-mod tests {
-    use super::regex;
+mod path_tests {
+    use super::path;
 
     const PATH:&'static str = "/Hello/:World";
     const QUOTES_PATH:&'static str = "/:\"Good Bye\"";
 
     #[test]
     fn lexer_test() {
-        let mut it = regex::lexer(PATH);
+        let mut it = path::lexer(PATH);
         assert_eq!(it.tokens.len(), 9);
 
-        it = regex::lexer(QUOTES_PATH);
+        it = path::lexer(QUOTES_PATH);
         assert_eq!(it.tokens.len(), 3);
     }
 
     #[test]
     fn iter_test() {
-        let mut it = regex::lexer(PATH);
-        let mut seg = it.parse(regex::TokenType::End);
+        let mut it = path::lexer(PATH);
+        let mut seg = it.parse(path::TokenType::End);
 
         assert_eq!(seg.len(), 2);
         while let Some(value) = seg.pop_front() {
             match value {
-                regex::Segment::Parameter(value) => {
+                path::Segment::Parameter(value) => {
                     assert_eq!(value, "World")
                 },
-                regex::Segment::Text(value) => {
+                path::Segment::Text(value) => {
                     assert_eq!(value, "Hello")
                 },
                 _ => { panic!("Unexpected Segment Type!")}
@@ -53,7 +67,7 @@ mod tests {
 
     #[test]
     fn compile_test() {
-        let (regex_str, keys) = regex::compile(PATH, false, true);
+        let (regex_str, keys) = path::compile(PATH, false, true);
         println!("{}", regex_str);
         assert_eq!(keys.len(), 1);
     }
