4d647ffc6eeee28ae71563c1e0c2c36f2bc3d225
Alex Malotky <ZimTKE@gmail.com>
2025-08-25 11:10:32 -0500

    updated mods / exports after move.

--- http/src/lib.rs
+++ http/src/lib.rs
@@ -1,19 +1,7 @@
 #![feature(str_from_raw_parts)]
 #[allow(dead_code)]
 
-mod path;
-pub use path::Path;
-pub mod request;
-pub use request::Request;
-mod response;
-pub use response::Response;
 pub mod types;
-pub use types::{Url, Method, Result, HttpStatus};
-mod error;
-pub use error::{HttpError, HttpErrorKind};
-mod headers;
-pub use headers::Headers;
+pub use types::*;
+mod server;
 
-pub trait Router {
-    fn handle<'a>(&self, url:&'a str) -> Result<Option<Response>, HttpError>;
-}
