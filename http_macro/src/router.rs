5eb5bae44a0855104a55b7c119f5b5df21afa62c
Alex Malotky <ZimTKE@gmail.com>
2025-08-25 10:40:10 -0500

    Updated router implementation to match changes to http types.

--- http_macro/src/router.rs
+++ http_macro/src/router.rs
@@ -11,7 +11,7 @@ pub(crate) struct RouterAttributes {
     path_trailing:bool,
     //default = true
     path_insensitive:bool,
-    //default == "All"
+    //default = "All"
     methods:String
     
 }
@@ -54,8 +54,9 @@ pub fn build_router(args:RouterAttributes, handler:syn::ItemFn) -> Result<TokenS
 
     let hand_attr:Vec<_> = handler.sig.inputs.iter().collect();
     let hand_block = handler.block;
+    let hand_return = handler.sig.output;
 
-    //panic!("{:?}", hand_attr);
+    //panic!("{:?}", hand_return);
 
     Ok(
         quote! {
@@ -85,19 +86,16 @@ pub fn build_router(args:RouterAttributes, handler:syn::ItemFn) -> Result<TokenS
                     }
                 }
 
-                fn handler(&self, #(#hand_attr),* ) {
+                fn handler(&self, #(#hand_attr),* ) #hand_return{
                     #hand_block
                 }
             }
 
             impl http::Router for #name {
-                fn handle<'a>(&self, url:&'a str) -> Option<()> {
-                    match self.match_path(url) {
-                        Some(param) => {
-                            self.handler(param);
-                            Some(())
-                        },
-                        None => None
+                fn handle<'a>(&self, req:&'a mut http::RequestBuilder<'a>) -> Result<Option<http::Response>, http::HttpError> {
+                    match self.match_path(&req.url.pathname()) {
+                        Some(param) => self.handler(req.build(param)).map(|rsp|Some(rsp)),
+                        None => Ok(None)
                     }
                 }
             }
