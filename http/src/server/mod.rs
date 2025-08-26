69d510426ddcd169ed8a75bde0dbec9c65a62197
Alex Malotky <ZimTKE@gmail.com>
2025-08-26 11:21:32 -0500

    created write_response, and updated build_request funcitons.

--- http/src/server/mod.rs
+++ http/src/server/mod.rs
@@ -1,10 +1,10 @@
-use crate::RequestBuilder;
-use std::net::TcpStream;
+use crate::{RequestBuilder, Response, Version};
+use std::{io::Read, net::TcpStream};
 
 mod http0;
 mod http1;
 
-enum RequestError {
+pub enum RequestError {
     ParseError(String),
     IoError(std::io::Error)
 }
@@ -13,7 +13,7 @@ pub fn load_settings(_path:&'static str) -> (u16, String){
     todo!("Ability to load from settings file.")
 }
 
-pub fn build_request(stream:TcpStream, hostname:&str, port:u16) -> Result<RequestBuilder, RequestError> {
+pub fn build_request<S>(stream:S, hostname:&str, port:u16) -> Result<RequestBuilder<S>, RequestError> where S: Read{
     match http1::parse_request(stream, hostname, port) {
         Ok(builder) => Ok(builder),
         Err(e) => match e {
@@ -24,4 +24,41 @@ pub fn build_request(stream:TcpStream, hostname:&str, port:u16) -> Result<Reques
             err => Err(RequestError::ParseError(format!("{}", err)))
         }
     }
+}
+
+pub fn write_connection_response(stream:&mut TcpStream, response: Response) -> Result<(), std::io::Error> {
+    write_response(stream, response, Version {
+        major: 1,
+        minor: 1
+    })
+}
+
+pub fn write_response(stream:&mut TcpStream, response:Response, version:Version) -> Result<(), std::io::Error> {
+    match version.major {
+        0 => http0::write(response, stream),
+        _ => http1::write_response(response, version, stream)
+    }
+}
+
+pub fn get_arguments() -> (Option<u16>, Option<String>) {
+    let mut port = None;
+    let mut hostname = None;
+
+    for input in std::env::args() {
+        let input: Vec<_> = input.split("=").collect();
+
+        match input[0].to_ascii_lowercase().as_str() {
+            "port" => {
+                port = Some(input.get(1).unwrap().parse().unwrap())
+            },
+            "hostname" => {
+                hostname = Some(input.get(1).unwrap().to_string())
+            },
+            key => {
+                panic!("Unknown command line argument: {key}!")
+            }
+        }
+    }
+
+    (port, hostname)
 }
