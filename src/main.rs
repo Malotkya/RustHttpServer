54ba9fba488e817b34989f24d735d150ca093f3f
Alex Malotky <ZimTKE@gmail.com>
2025-08-27 15:02:18 -0500

    More testing of server.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 pub use http_macro::{router, server};
 
 #[router(path="/Hello/:Name")]
-async fn TestName<'b>(req: http::Request<'b, impl std::io::Read, TestNamePathParam<'b>>) -> Result<http::Response, http::HttpError> {
+async fn TestName(req: http::Request<TestNamePathParam>) -> Result<http::Response, http::HttpError> {
     Ok(http::Response::from(format!("Hello {}!", req.param.Name)))
+}
+
+#[router(path="/")]
+async fn Home(_: http::Request<HomePathParam>) -> http::Result {
+    Ok(http::Response::from("Hello World!"))
 }
 
 #[server(5000)]
 struct ServerName (
+    Home,
     TestName
 );
 
