746d7284d9c877b9bd337e1edf8765ae4be54cd8
Alex Malotky <ZimTKE@gmail.com>
2025-08-29 11:26:37 -0500

    Updated headers to accept strict types.

--- http/src/types/headers/mod.rs
+++ http/src/types/headers/mod.rs
@@ -10,7 +10,7 @@ use std::collections::hash_map::Iter;
 
 mod value;
 mod name;
-//mod types;
+mod types;
 
 pub struct Headers(HashMap<HeaderName, HeaderValue>);
 
@@ -20,11 +20,20 @@ impl Headers {
         Self(HashMap::new())
     }
 
-    pub fn set(&mut self,key:&str, value:&str) {
+    pub fn set<V: Into<HeaderValue>>(&mut self,key:&str, value:V) -> &mut Self {
         self.0.insert(
             HeaderName::from(key),
-            HeaderValue::from(value)
+            value.into()
         );
+        self
+    }
+
+    pub fn insert<H: HttpHeader+Into<HeaderValue>>(&mut self, value:H) -> &mut Self {
+        self.0.insert(
+            H::name(),
+            value.into()
+        );
+        self
     }
 
     pub fn get(&self, key:&str) -> Option<&HeaderValue> {
@@ -49,4 +58,8 @@ impl<'a> IntoIterator for &'a Headers {
     fn into_iter(self) -> Self::IntoIter {
         self.0.iter()
     }
+}
+
+pub trait HttpHeader {
+    fn name() -> HeaderName;
 }
