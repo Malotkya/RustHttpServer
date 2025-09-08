a3fba1f59e664fa7109dfb64396f44f03592a584
Alex Malotky <ZimTKE@gmail.com>
2025-09-08 15:43:28 -0500

    Added pipability to AsyncBuf Writter/Reader

--- async_lib/src/future/io/buffer/read.rs
+++ async_lib/src/future/io/buffer/read.rs
@@ -1,12 +1,16 @@
-use crate::future::io::AsyncRead;
-use std::io;
+use crate::future::io::PollRead;
+use crate::io::stream::{Stream, SourcePipe, Pipe};
+use std::{
+    io,
+    task::{Context, Poll}
+};
 
-pub struct AsyncBufReader<R: AsyncRead> {
+pub struct AsyncBufReader<R: PollRead> {
     buf: super::AsyncBuffer,
     inner: R
 }
 
-impl<R: AsyncRead> AsyncBufReader<R> {
+impl<R: PollRead> AsyncBufReader<R> {
     pub fn new(inner: R) -> Self {
         Self {
             buf: super::AsyncBuffer::default(),
@@ -14,22 +18,93 @@ impl<R: AsyncRead> AsyncBufReader<R> {
         }
     }
 
-    async fn read(&mut self, buf:&mut [u8]) -> io::Result<usize> {
-        self.buf.read_more(&mut self.inner).await?;
-        self.buf.read(buf).await
+    fn poll_read(&mut self, cx: &mut Context<'_>, buf:&mut [u8]) -> Poll<io::Result<usize>> {
+        if self.buf.poll_read_more(cx, &mut self.inner).is_ready() {
+            self.buf.poll_read(cx, buf)
+        } else {
+            Poll::Pending
+        }
     }
 
-    async fn read_vectored(&mut self, bufs: &mut [io::IoSliceMut<'_>]) -> io::Result<usize> {
-        self.buf.read_more(&mut self.inner).await?;
-        self.buf.read_vectored(bufs).await
+    fn read(&mut self, buf:&mut [u8]) -> impl Future<Output = io::Result<usize>> {
+        std::future::poll_fn(move |cx|{
+            self.poll_read(cx, buf)
+        })
     }
 
-    async fn fill_buf(&mut self) -> io::Result<&[u8]> {
-        self.buf.read_more(&mut self.inner).await?;
-        Ok(self.buf.buffer())
+    fn poll_read_vectored(&mut self, cx: &mut Context<'_>, bufs: &mut [io::IoSliceMut<'_>]) -> Poll<io::Result<usize>> {
+        if self.buf.poll_read_more(cx, &mut self.inner).is_ready() {
+            self.buf.poll_read_vectored(cx, bufs)
+        } else {
+            Poll::Pending
+        }
+    }
+
+    fn read_vectored(&mut self, bufs: &mut [io::IoSliceMut<'_>]) -> impl Future<Output = io::Result<usize>> {
+        std::future::poll_fn(move |cx|{
+            self.poll_read_vectored(cx, bufs)
+        })
+    }
+
+    fn poll_fill_buf(&mut self, cx:&mut Context<'_>) -> Poll<io::Result<&[u8]>> {
+        if self.buf.poll_read_more(cx, &mut self.inner).is_ready() {
+            Poll::Ready(Ok(self.buf.buffer()))
+        } else {
+            Poll::Pending
+        }
+    }
+
+    fn fill_buf(&mut self) -> impl Future<Output = io::Result<&[u8]>> {
+        let future = std::future::poll_fn(|cx|{
+            self.poll_fill_buf(cx).map(|result|result.map(|ptr|(ptr.as_ptr() as usize, ptr.len())))
+        });
+
+        let combine = async||{
+            future.await.map(|(ptr, len)| unsafe{ std::slice::from_raw_parts(ptr as *const u8, len)})
+        };
+
+        combine()
     }
 
     fn consume(&mut self, amt: usize) {
         self.buf.consume(amt)
+    }
+}
+
+impl<R: PollRead> Stream for AsyncBufReader<R> {
+    type Item = Vec<u8>;
+
+    fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+
+        if let Poll::Ready(r) = self.buf.poll_read_more(cx, &mut self.inner) {
+            r.unwrap();
+            let size = self.buf.size();
+            if size == 0 {
+                Poll::Ready(None)
+            } else {
+                let vec = unsafe{
+                    let slice = self.buf.mut_buffer();
+                    Vec::from_parts(
+                        std::ptr::NonNull::new_unchecked(slice.as_mut_ptr()),
+                        size, 
+                        size
+                    )
+                };
+                self.buf.consume(size);
+                Poll::Ready(Some(
+                    vec
+                ))
+            }
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+impl<R: PollRead + 'static> SourcePipe for AsyncBufReader<R> {
+    type Chunk = Self::Item;
+
+    fn pipe<P: crate::io::stream::TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>> + 'static>(&mut self, pipe: &mut P) -> crate::io::stream::Pipe<P, Self> {
+        Pipe::new(self, pipe)
     }
 }
--- async_lib/src/future/io/buffer/write.rs
+++ async_lib/src/future/io/buffer/write.rs
@@ -1,13 +1,20 @@
-use crate::future::io::AsyncWrite;
-use std::io;
+use crate::future::io::{
+    stream::{Sink, TargetPipe},
+    PollWrite,
+    Stream
+};
+use std::{
+    io,
+    task::{Context, Poll}
+};
 
-pub struct AsyncBufWritter<W: AsyncWrite> {
+pub struct AsyncBufWritter<W: PollWrite> {
     inner: W,
     buf: Vec<u8>,
     panicked: bool
 }
 
-impl<W: AsyncWrite> AsyncBufWritter<W> {
+impl<W: PollWrite> AsyncBufWritter<W> {
     pub fn new(inner: W) -> Self {
         Self {
             buf: Vec::with_capacity(super::DEFAULT_BUFFER_SIZE),
@@ -16,38 +23,54 @@ impl<W: AsyncWrite> AsyncBufWritter<W> {
         }
     }
 
-    async fn flush_buf(&mut self) -> io::Result<()> {
+    fn poll_flush_buf(&mut self, cx:&mut Context<'_>) -> Poll<io::Result<()>> {
         let mut guard = BufGuard::new(&mut self.buf);
         while !guard.done() {
             self.panicked = true;
-            let r = self.inner.write(guard.remaining()).await;
+            let r = match self.inner.poll_write(cx, guard.remaining()) {
+                Poll::Pending => return Poll::Pending,
+                Poll::Ready(r) => r
+            };
             self.panicked = false;
 
             match r {
-                Ok(0) => return Err(io::Error::new(
+                Ok(0) => return Poll::Ready(Err(io::Error::new(
                     io::ErrorKind::WriteZero,
                     "failed to write the buffered data"
-                )),
+                ))),
                 Ok(n) => guard.consume(n),
-                Err(e) => return Err(e)
+                Err(e) => return Poll::Ready(Err(e))
             }
         }
 
-       Ok(())
+        guard.clear();
+       Poll::Ready(Ok(()))
     }
 
-    async fn write_to_buf(&mut self, buf: &[u8]) -> usize {
+    fn flush_buf(&mut self) -> impl Future<Output=io::Result<()>> {
+        std::future::poll_fn(|cx|{
+            self.poll_flush_buf(cx)
+        })
+    }
+
+    pub fn poll_write_to_buf(&mut self, _cx:&mut Context<'_>, buf: &[u8]) -> Poll<usize> {
         let available = self.spare_capacity();
         let amt_to_buffer = available.min(buf.len());
 
         unsafe {
-            self.write_to_buffer_unchecked(&buf[..amt_to_buffer]).await;
+            self.write_to_buffer_unchecked(&buf[..amt_to_buffer]);
         }
 
-        amt_to_buffer
+        Poll::Ready(amt_to_buffer)
     }
 
-    async unsafe fn write_to_buffer_unchecked(&mut self, buf: &[u8]) {
+    fn write_to_buffer(&mut self, buf: &[u8]) -> impl Future<Output = usize> {
+        std::future::poll_fn(|cx|{
+            self.poll_write_to_buf(cx, buf)
+        })
+    }
+
+    unsafe fn write_to_buffer_unchecked(&mut self, buf: &[u8]) {
         debug_assert!(buf.len() <= self.spare_capacity());
         let old_len = self.buf.len();
         let buf_len: usize = buf.len();
@@ -67,6 +90,51 @@ impl<W: AsyncWrite> AsyncBufWritter<W> {
 
     fn spare_capacity(&self) -> usize {
         self.buf.capacity() - self.buf.len()
+    }
+}
+
+impl<W: PollWrite> TargetPipe for AsyncBufWritter<W> {
+    type Chunk = Vec<u8>;
+    type Error = io::Error;
+
+    fn poll_accept_next(&mut self, ctx: &mut Context<'_>, chunk:Self::Chunk) -> Poll<Result<(), Self::Error>> {
+        match self.poll_write_to_buf(ctx, &chunk) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(size) => if size < chunk.len() {
+                Poll::Ready(Err(
+                    io::Error::new(
+                        io::ErrorKind::OutOfMemory,
+                        "Write Buffer has run out of memory!"
+                    )
+                ))
+            } else {
+                Poll::Ready(Ok(()))
+            }
+        }
+    }
+
+    fn poll_flush(&mut self, ctx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.poll_flush_buf(ctx)
+    }
+}
+
+impl<W: PollWrite> Sink for AsyncBufWritter<W> {
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        if self.panicked {
+            Poll::Ready(Err(
+                io::Error::new(
+                    io::ErrorKind::BrokenPipe,
+                    "Sink has panicked"
+                )
+            ))
+        } else {
+            Poll::Ready(Ok(()))
+        }
+            
+    }
+
+    fn poll_close(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.poll_flush(cx)
     }
 }
 
@@ -93,6 +161,10 @@ impl<'a> BufGuard<'a> {
 
     fn done(&self) -> bool {
         self.written >= self.buffer.len()
+    }
+
+    fn clear(&mut self) {
+        self.buffer.clear()
     }
 }
 
