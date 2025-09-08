6a086c7f5f347e08f26bf37c0076bcbdd71e8683
Alex Malotky <ZimTKE@gmail.com>
2025-09-08 15:54:54 -0500

    Updated unsafe features and exports.

--- async_lib/src/lib.rs
+++ async_lib/src/lib.rs
@@ -1,21 +1,13 @@
+#![feature(box_vec_non_null)]
+#![feature(associated_type_defaults)]
+#![feature(maybe_uninit_slice)]
 #![feature(trait_alias)]
 #![allow(dead_code)]
-use std::cell::RefCell;
-use executor::Executor;
 
-mod executor;
+mod event;
+pub use event::EventEmitter;
+pub mod executor;
 mod future;
 pub use future::*;
 
-const EXECUTOR_QUEUE_SIZE: usize = 1000;
-
-thread_local! {
-    static EXECUTOR:RefCell<Executor<'static>> = RefCell::new(Executor::new(EXECUTOR_QUEUE_SIZE))
-}
-
-pub(crate) fn spawn_task(future: impl Future<Output = ()> + 'static) {
-    EXECUTOR.with(move |cell|{
-        let mut exe = cell.borrow_mut();
-        exe.spawn_task(future);
-    })
-}
+pub(crate) use executor::spawn_task;
