dcd123026f222381bf519fa0bdf39bdce15d8599
Alex Malotky <ZimTKE@gmail.com>
2025-09-08 13:26:32 -0500

    Remove lifetimes from pipe.

--- async_lib/src/future/io/stream/pipe/source.rs
+++ async_lib/src/future/io/stream/pipe/source.rs
@@ -12,31 +12,31 @@ use super::{TargetPipe, Pipe};
 pub trait SourcePipe: Stream + Sized {
     type Chunk = Self::Item;
 
-    fn poll<'a, P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>>>(&'a mut self, pipe: &'a mut P) -> Pipe<'a, P, Self>;
+    fn pipe<P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>> + 'static>(&mut self, pipe: &mut P) -> Pipe<P, Self>;
 }
 
-impl<T> SourcePipe for Vec<T> {
-    fn poll<'a, P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>>>(&'a mut self, pipe: &'a mut P) -> Pipe<'a, P, Self> {
+impl<T: 'static> SourcePipe for Vec<T> {
+    fn pipe<P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>> + 'static>(&mut self, pipe: &mut P) -> Pipe<P, Self> {
         Pipe::new(self, pipe)
     }
 }
 
-impl<T> SourcePipe for VecDeque<T> {
-    fn poll<'a, P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>>>(&'a mut self, pipe: &'a mut P) -> Pipe<'a, P, Self> {
+impl<T: 'static> SourcePipe for VecDeque<T> {
+    fn pipe<P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>> + 'static>(&mut self, pipe: &mut P) -> Pipe<P, Self> {
         Pipe::new(self, pipe)
     }
 }
 
-impl<S: ?Sized + Stream + Unpin> SourcePipe for &mut S {
+/*impl<S: ?Sized + Stream + Unpin> SourcePipe for &mut S {
 
-    fn poll<'a, P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>>>(&'a mut self, pipe: &'a mut P) -> Pipe<'a, P, Self> {
+    fn pipe<P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>> + 'static>(&mut self, pipe: &mut P) -> Pipe<P, Self> {
         Pipe::new(self, pipe)
     }
-}
+}*/
 
-impl<S: ?Sized + Stream + Unpin> SourcePipe for Box<S> {
+impl<S: ?Sized + Stream + Unpin + 'static> SourcePipe for Box<S> {
 
-    fn poll<'a, P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>>>(&'a mut self, pipe: &'a mut P) -> Pipe<'a, P, Self> {
+    fn pipe<P: TargetPipe<Chunk: From<<Self as SourcePipe>::Chunk>> + 'static>(&mut self, pipe: &mut P) -> Pipe<P, Self> {
         Pipe::new(self, pipe)
     }
 }
