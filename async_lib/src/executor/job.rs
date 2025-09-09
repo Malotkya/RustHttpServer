eb5d14698f784b0823c1a86d40a98a4c3a94b37b
Alex Malotky <ZimTKE@gmail.com>
2025-09-09 12:54:16 -0500

    Created single thread JobHandler that combines the listener task with io tasks.

--- async_lib/src/executor/job.rs
+++ async_lib/src/executor/job.rs
@@ -1,12 +1,13 @@
 use super::{AtomicQueue, AtomicOption, ThreadJob, ThreadPoolConnection};
 use std::thread;
+use std::sync::Arc;
 
 pub(crate) type Job = Box<dyn FnOnce() + Send + 'static>;
 
 #[derive(Clone)]
 pub(crate) struct JobHandler {
-    queue: AtomicQueue<Job>,
-    conn: AtomicOption<ThreadPoolConnection>
+    pub(crate) queue: AtomicQueue<Job>,
+    pub(crate) conn: AtomicOption<ThreadPoolConnection>
 }
 
 impl JobHandler {
@@ -20,6 +21,15 @@ impl JobHandler {
     pub fn add(&self, job: impl FnOnce() + Send + 'static) {
         self.queue.push(Box::new(job));
     }
+
+    pub fn single_thread(&'static self, listener: impl Fn() + Send + Sync + 'static) -> Arc<SingleThreadJob> {
+        Arc::new(
+            SingleThreadJob {
+                handler: self,
+                listener: Arc::new(Box::new(listener))
+            }
+        )
+    }
 }
 
 impl ThreadJob for JobHandler {
@@ -32,6 +42,34 @@ impl ThreadJob for JobHandler {
             job();
         } else {
             thread::park();
+        }
+    }
+}
+
+pub(crate) struct SingleThreadJob {
+    handler: &'static JobHandler,
+    listener: Arc<Box<dyn Fn() + Send + Sync + 'static>>
+}
+
+impl Clone for SingleThreadJob {
+    fn clone(&self) -> Self {
+        Self {
+            handler: self.handler,
+            listener: self.listener.clone()
+        }
+    }
+}
+
+impl ThreadJob for SingleThreadJob {
+    fn connect(&self, conn: ThreadPoolConnection) {
+        self.handler.conn.set(Some(conn))
+    }
+
+    fn next(&self) {
+        (self.listener)();
+
+        if let Some(job) = self.handler.queue.pop() {
+            job();
         }
     }
 }
