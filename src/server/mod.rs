558357ca8179ef355253ee945a0243808b59bba7
Alex Malotky <ZimTKE@gmail.com>
2025-09-09 21:59:34 -0500

    Deleted older Async Executor and SyncServer.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,28 +1,133 @@
+use http_types::{RequestBuilder, Response, Version, HttpError, HttpErrorKind};
 use crate::{
     http0, http1,
-    RequestBuilder, Response, Version, HttpError, HttpErrorKind
+    
 };
 use std::{
     io::Read,
-    net::TcpStream
+    net::{TcpStream, TcpListener},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        mpsc, Arc
+    },
+    thread
 };
-pub use async_server::*;
-pub use sync_server::*;
 
-pub(crate) mod executor;
-pub(crate) mod task;
-pub(crate) mod queue;
-mod async_server;
-mod sync_server;
+mod atomics;
 
 pub trait ServerParts {
     fn new() -> Self;
     fn hostname(&self) -> &str;
     fn port(&self) -> &u16;
+    fn handle_request(&self, req:&mut RequestBuilder<TcpStream>) -> impl Future<Output = Response>;
 }
 
-pub trait Server {
-    fn start() -> std::io::Result<()>;
+pub struct Server<PARTS: ServerParts> {
+    pub(crate) parts: PARTS,
+}
+
+impl<P: ServerParts> Server<P> {
+    pub fn new(parts:P) -> Self {
+        Self{
+            parts,
+        }
+    }
+
+    pub fn port(&self) -> &u16 {
+        self.parts.port()
+    }
+
+    pub fn hostname(&self) -> &str {
+        self.parts.hostname()
+    }
+
+    pub fn spawn_thread(&self) -> std::io::Result<atomics::ListenerHandle> {
+        let (conn, conn_recv) = mpsc::channel::<TcpStream>();
+        let address = Arc::new(self.to_string());
+        let close = Arc::new(AtomicBool::new(false));
+        let (ready_1, ready_2) = atomics::Ready::new();
+
+        let handle = atomics::ListenerHandle(
+            close.clone(),
+            thread::spawn(move||{
+                let listener = match TcpListener::bind(address.as_str()){
+                    Ok(tcp) => tcp,
+                    Err(e) => {
+                        ready_1.set_error(e);
+                        return;
+                    }
+                };
+                
+                if let Err(e) = listener.set_nonblocking(true) {
+                    ready_1.set_error(e);
+                    return;
+                } else {
+                    ready_1.set_success();
+                }
+                println!("Listening at: http://{}", address.as_str());
+
+                loop {
+                    match listener.accept() {
+                        Ok(connection) => {
+                            if conn.send(connection.0).is_err() {
+                                println!("Lost connection with main thread!");
+                                break;
+                            }
+                        },
+                        Err(e) => {
+                            if e.kind() != std::io::ErrorKind::WouldBlock {
+                                println!("{}", e)
+                            }
+                        }
+                    }
+
+                    if close.load(Ordering::Relaxed) {
+                        break;
+                    }
+                }
+
+                println!("Stoping listener thread.");
+            }),
+            conn_recv
+        );
+
+        ready_2.wait().map(|_|handle)
+    }
+
+    /*fn start() -> std::io::Result<()> {
+        let server = Self::new(P::new());
+        let mut exec: Executor = Executor::new();
+        let hook = server.spawn_thread()?;
+
+        loop {
+            match hook.conn() {
+                Ok(stream) => {
+                    exec.spawn(Task::new(
+                        handle_connection(&server.parts, stream)
+                    ));
+                },
+                Err(e) => {
+                    if e == atomics::ConnectionError::Disconnected {
+                        println!("Lost connection with listener thread!\nAtempting to shutdown gracefully.");
+                        hook.wait_close().expect("Unable to wait for thread to close!");
+                        break;
+                    }
+                }
+            }
+
+            exec.run_ready_tasks();
+            //exec.sleep_if_idle();
+        }
+
+        println!("Good bye!");
+        Ok(())
+    }*/
+}
+
+impl<P:ServerParts> ToString for Server<P> {
+    fn to_string(&self) -> String {
+        format!("{}:{}", self.parts.hostname(), *self.parts.port())
+    }
 }
 
 enum ConnectionError {
@@ -145,4 +250,20 @@ pub fn get_arguments() -> (Option<u16>, Option<String>, Option<String>) {
     }
 
     (port, hostname, config_file)
+}
+
+async fn handle_connection<P:ServerParts>(parts:&P, stream:TcpStream) -> super::task::Result {
+    let (req_stream, mut resp_stream) = super::build_connections(stream)?;
+
+    if let Some(mut request) = super::build_request(req_stream, &mut resp_stream, parts.hostname(), *parts.port())? {
+        let response = parts.handle_request(&mut request).await;
+        crate::log(&request, &response);
+        crate::write_response(
+            &mut resp_stream,
+            response,
+            request.version
+        )?;
+    }
+
+    Ok(())      
 }
