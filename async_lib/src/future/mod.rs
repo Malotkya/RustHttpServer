f6b8070be21e8ea1e3663eeaa968c409ec59e37c
Alex Malotky <ZimTKE@gmail.com>
2025-09-10 10:05:24 -0500

    Created clone_io_result helper function.

--- async_lib/src/future/mod.rs
+++ async_lib/src/future/mod.rs
@@ -3,3 +3,15 @@ pub mod io;
 pub mod net;
 mod promise;
 pub use promise::Promise;
+
+pub(crate) fn clone_io_result<T: Clone>(result: &io::Result<T>) -> io::Result<T> {
+    match result {
+        Ok(t) => Ok(t.clone()),
+        Err(e) => Err(
+            std::io::Error::new(
+                e.kind(),
+                e.to_string()
+            )
+        )
+    }
+}
--- async_lib/src/future/net/mod.rs
+++ async_lib/src/future/net/mod.rs
@@ -1,4 +1,5 @@
 pub use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4};
+pub(crate) use super::clone_io_result;
 
 mod tcp;
 pub use tcp::*;
