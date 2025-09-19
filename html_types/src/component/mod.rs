79133f706457885246ace3db07dde5b863e9e265
Alex Malotky <ZimTKE@gmail.com>
2025-09-19 16:19:43 -0500

    Update mods.

--- html_types/src/component/mod.rs
+++ html_types/src/component/mod.rs
@@ -2,6 +2,8 @@
 //pub mod elements;
 
 pub(crate) mod node;
-pub(crate) mod element;
 pub(crate) mod attributes;
 pub use attributes::{AttributeName, AttributeValue};
+pub(crate) mod document;
+pub(crate) mod element;
+pub(crate) mod other;
