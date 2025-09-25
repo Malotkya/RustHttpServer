04dc9ad97887f5710aa62c03930342b0eacdb1d6
Alex Malotky <ZimTKE@gmail.com>
2025-09-25 10:21:39 -0500

    Fixed macro so function accepts reference.

--- html_types/src/component/document/macros.rs
+++ html_types/src/component/document/macros.rs
@@ -1,6 +1,6 @@
 macro_rules! GenerateNodeFunctions {
     ( $( ($func:ident, $name:ident) ),+ ) => {  $( paste::paste!{
-        pub fn $func (self: Rc<Self>, mut data:[<$name Data>]) -> DocumentItemRef<[<$name Data>]> {
+        pub fn $func (self: &Rc<Self>, mut data:[<$name Data>]) -> DocumentItemRef<[<$name Data>]> {
             let list = &self.all_nodes as *const NodeArray as *mut NodeArray;
             
             let ptr_data = std::ptr::from_mut(&mut data);
