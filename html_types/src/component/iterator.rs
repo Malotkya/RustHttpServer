de5f5a3c9f576d9b4bc923592042e38dd4ed4e9a
Alex Malotky <ZimTKE@gmail.com>
2025-09-25 12:15:39 -0500

    Updated Iterators with helper functions and traits.

--- html_types/src/component/iterator.rs
+++ html_types/src/component/iterator.rs
@@ -5,43 +5,90 @@ use std::{
 use crate::component::{
     document::{DocumentData, NodeDocumentItemRef},
     element::Element,
-    node::Node
+    attributes::Attribute,
+    node::{
+        Node,
+        IntoNode
+    }
 };
 
-pub enum IteratorType<'d> {
+pub(crate) enum IteratorType<'d> {
     Document(Iter<'d, (usize, usize)>, Rc<DocumentData>),
     Node(Iter<'d, Node>),
     None
 }
 
+impl<'d> IteratorType<'d> {
+    fn is_none(&self) -> bool {
+        match self {
+            Self::None => true,
+            _ => false
+        }
+    }
+}
+
 pub(crate) struct InternalIterator<'d>(IteratorType<'d>);
 
+impl<'d> InternalIterator<'d> {
+    fn is_void(&self) -> bool {
+        self.0.is_none()
+    }
+}
+
 impl<'d> Iterator for InternalIterator<'d> {
-    type Item = NodeDocumentItemRef;
+    type Item = Node;
 
     fn next(&mut self) -> Option<Self::Item> {
         match &mut self.0 {
             IteratorType::Document(it, doc) => 
                 it.next().map(|(outer, inner)|{
                     doc.all_nodes.get(*outer, *inner).map(|item|{
-                        NodeDocumentItemRef::new(
+                        Node(NodeDocumentItemRef::new(
                             doc.clone(),
                             item
-                        )
+                        ))
                     })
                 }).flatten(),
             IteratorType::Node(it) => {
-                it.next().map(|node|node.0.clone())
+                it.next().map(|node|node.node())
             },
             IteratorType::None => None
         }
     }
 }
 
+impl<'d> DoubleEndedIterator for InternalIterator<'d> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        match &mut self.0 {
+            IteratorType::Document(it, doc) => 
+                it.next_back().map(|(outer, inner)|{
+                    doc.all_nodes.get(*outer, *inner).map(|item|{
+                        Node(NodeDocumentItemRef::new(
+                            doc.clone(),
+                            item
+                        ))
+                    })
+                }).flatten(),
+            IteratorType::Node(it) => {
+                it.next_back().map(|node|node.node())
+            },
+            IteratorType::None => None
+        }
+    }
+}
+
+impl<'d> ExactSizeIterator for InternalIterator<'d> {
+    fn len(&self) -> usize {
+        match &self.0 {
+            IteratorType::Document(it, _) => it.len(),
+            IteratorType::Node(it) => it.len(),
+            IteratorType::None => 0
+        }
+    }
+}
+
 macro_rules! BuildIterator {
-    ($name:ident{
-        $($body:tt)*
-    }) => {
+    ($name:ident($type:path)=$convert:expr) => {
         pub struct $name<'d>(InternalIterator<'d>);
 
         impl<'d> $name<'d> {
@@ -62,22 +109,62 @@ macro_rules! BuildIterator {
                     IteratorType::None
                 ))
             }
+
+            pub fn is_void(&self) -> bool {
+                self.0.is_void()
+            }
         }
 
         impl<'d> Iterator for $name<'d> {
-            type Item = Element;
+            type Item = $type;
 
             fn next(&mut self) -> Option<Self::Item> {
-                $($body)*
+                while let Some(next) = self.0.next() {
+                    if let Some(value) = $convert(next) {
+                        return Some(value)
+                    }
+                }
+
+                None
+            }
+
+            fn last(mut self) -> Option<Self::Item> {
+                while let Some(back) = self.0.next_back() {
+                    if let Some(value) = $convert(back) {
+                        return Some(value)
+                    }
+                }
+
+                None
+            }
+        }
+
+        impl<'d> ExactSizeIterator for $name<'d> {
+            fn len(&self) -> usize {
+                self.0.len()
             }
         }
     };
 }
 
-BuildIterator!(ChildIterator{
-    todo!()
+BuildIterator!(NodeIterator(Node)=|n:Node| -> Option<Node>{
+    if n.is_visual_element() {
+        Some(n)
+    } else {
+        None
+    }
 });
 
-BuildIterator!(AttributeIterator{
-    todo!()
+impl<'a> Into<NodeIterator<'a>> for ChildIterator<'a> {
+    fn into(self) -> NodeIterator<'a> {
+        NodeIterator(self.0)
+    }
+}
+
+BuildIterator!(ChildIterator(Element)=|n:Node| -> Option<Element>{
+    TryInto::<Element>::try_into(n).ok()
 });
+BuildIterator!(AttributeIterator(Attribute)=|n:Node| -> Option<Attribute> {
+    TryInto::<Attribute>::try_into(n).ok()
+});
+
