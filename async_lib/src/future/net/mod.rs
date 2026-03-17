6f90b22831ee43b881a1dde657a6dcd69c370694
Alex M <ZimTKE@gmail.com>
2026-03-17 14:57:33 -0500

    Readded tcp_listener_thread function (makes server code easier to read)

--- async_lib/src/future/net/mod.rs
+++ async_lib/src/future/net/mod.rs
@@ -1,6 +1,37 @@
+use crate::{
+    executor::{
+        Thread, is_running
+    },
+    io::{
+        ErrorKind, Result
+    }
+};
 
 pub use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4};
 pub(crate) use super::clone_io_result;
 
 mod tcp;
 pub use tcp::*;
+
+pub fn tcp_listener_thread(addr:String, callback: impl Fn(TcpStream) + Sync + Send + 'static) -> Result<impl Thread<()>> {
+    let mut listener = TcpListener::bind(addr.clone())?;
+
+    if listener.set_nonblocking(true).is_err() {
+        println!("Failed to set nonblocking on TcpListener!");
+    }
+
+    Ok(move ||{
+        println!("Listening at {}", addr);
+
+        while is_running() {
+            match listener.sync_accept() {
+                Ok(conn) => {
+                    callback(conn.0);
+                },
+                Err(e) => if e.kind() != ErrorKind::WouldBlock {
+                    println!("Connection Error: {}", e);
+                } 
+            }
+        }
+    })
+}
