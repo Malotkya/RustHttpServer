e73a0e3b5a697a76a5c78765d45777a0979b8ece
Alex M <ZimTKE@gmail.com>
2026-03-17 13:26:03 -0500

    Re-built the thread management to mirror the task handler.

--- async_lib/src/executor/mod.rs
+++ async_lib/src/executor/mod.rs
@@ -1,108 +1,39 @@
 use std::{
-    cell::RefCell,
     sync::{
         LazyLock,
         atomic::{AtomicBool, Ordering},
-        mpsc::{channel, Receiver, TryRecvError}
+        
     },
-    thread::JoinHandle
 };
 
 mod tasks;
 use tasks::*;
-mod waker;
 mod thread;
 pub(crate) use thread::*;
 mod atomic;
 pub(crate) use atomic::*;
 
-const DEFAULT_QUEUE_SIZE: usize = 1000;
+pub(crate) const DEFAULT_QUEUE_SIZE: usize = 1000;
 
-pub(crate) static RUNNING:AtomicBool = AtomicBool::new(false);
-static JOB_THREAD_QUEUE:LazyLock<AtomicQueue<Box<dyn ThreadJob>>> = LazyLock::new(||{
-    AtomicQueue::new("Job", DEFAULT_QUEUE_SIZE)
-});
-static ASYNC_TASKS_HANDLER:LazyLock<TaskHandler> = LazyLock::new(||{
-    TaskHandler::new(DEFAULT_QUEUE_SIZE)
-});
-static MAIN_THREAD_HANDLE:LazyLock<AtomicOption<JoinHandle<()>>> = LazyLock::new(||{
-    AtomicOption::none()
-});
+pub trait Thread<R> = FnOnce() -> R + Send + Sync + 'static;
 
-pub trait ThreadJobReturnValue<T> = Fn(ThreadPoolConnection) -> T + Send + Sync + 'static;
+pub(crate) static RUNNING: AtomicBool = AtomicBool::new(false);
+static TASK_MANAGER   :LazyLock<TaskHandler> = LazyLock::new(||TaskHandler::new(DEFAULT_QUEUE_SIZE));
+static THREAD_MANAGER :LazyLock<ThreadManager> = LazyLock::new(||ThreadManager::new(DEFAULT_QUEUE_SIZE)); 
 
-thread_local! {
-    static THREAD_POOL:RefCell<ThreadPool> = RefCell::new(ThreadPool::new());
-}
-
+#[inline]
 pub fn spawn_task(future: impl Future<Output = ()> + 'static) {
-    ASYNC_TASKS_HANDLER.spawn_task(future);
-    unpark_main_thread();
-}
-
-pub fn spawn_thread_job(func: impl ThreadJob) {
-    JOB_THREAD_QUEUE.push(Box::new(func));
-
-    unpark_job_threads(1);
-}
-
-pub fn await_thread_job<T: Send + 'static>(func: impl ThreadJobReturnValue<T>) -> impl Future<Output = T> {
-    let (sender, receiver) = channel::<T>();
-
-    JOB_THREAD_QUEUE.push(Box::new(move|conn|{
-        sender.send(func(conn)).unwrap()
-    }));
-
-    unpark_job_threads(1);
-
-    Actor(receiver)
-}
-
-struct Actor<T:Send>(pub(crate) Receiver<T>);
-
-impl<T:Send> Future for Actor<T> {
-    type Output = T;
-
-    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
-        match self.0.try_recv() {
-            Ok(value) => std::task::Poll::Ready(value),
-            Err(e) => if e == TryRecvError::Empty {
-                cx.waker().wake_by_ref();
-                std::task::Poll::Pending
-            } else {
-                panic!("Actor disconected!")
-            }
-        }
-    }
-}
-
-fn job_thread(conn:ThreadPoolConnection) {
-    while RUNNING.load(Ordering::Relaxed) {
-        //Sleep If Idle
-        if JOB_THREAD_QUEUE.is_empty() && ASYNC_TASKS_HANDLER.is_empty() {
-            std::thread::park();
-        }
-
-        while let Some(job) = JOB_THREAD_QUEUE.pop() {
-            job(conn.clone())
-        }
-
-        ASYNC_TASKS_HANDLER.run_next_task();
-    }
+    TASK_MANAGER.spawn_task(future);
 }
 
 #[inline]
-fn unpark_main_thread() {
-    if let Some(handle) = MAIN_THREAD_HANDLE.try_unwrap() {
-        handle.thread().unpark();
-    }
+pub fn queue_job<R:Send + 'static>(func: impl Thread<R>) -> impl Future<Output = R> {
+    THREAD_MANAGER.queue_thread(func)
 }
 
 #[inline]
-pub(crate) fn unpark_job_threads(count:usize) {
-    THREAD_POOL.with(|cell|{
-        cell.borrow_mut().unpark(count)
-    });
+pub fn is_running() -> bool {
+    RUNNING.load(Ordering::Relaxed)
 }
 
 pub fn init_async_thread_pool(thread_count:usize) {  
@@ -110,34 +41,17 @@ pub fn init_async_thread_pool(thread_count:usize) {
         panic!("Unable to initalize thread pool with zero threads!");
     }
     
-    THREAD_POOL.with(|cell|{
-        let mut pool = cell.borrow_mut();
-        pool.init(thread_count);
-
-        for i in 0..thread_count {
-            pool.init_thread(&i.to_string(), job_thread);
-        }
-    });
+    THREAD_MANAGER.init(thread_count);
 }
 
 pub fn start() {
     RUNNING.store(true, Ordering::Relaxed);
 
     while RUNNING.load(Ordering::Relaxed) {
-        //Sleep If Idle
-        if ASYNC_TASKS_HANDLER.is_empty() {
-            std::thread::park();
-
-        //Ask for help with tasks
-        } else {
-            unpark_job_threads(ASYNC_TASKS_HANDLER.len());
-        }
-
-        //Handle as many tasks as possible.
-        ASYNC_TASKS_HANDLER.run_all_tasks();
+        TASK_MANAGER.run_all_tasks();
     }
 
-    THREAD_POOL.take().join();
+    THREAD_MANAGER.join_all();
 }
 
 pub fn shut_down() {
--- async_lib/src/executor/thread.rs
+++ async_lib/src/executor/thread.rs
@@ -1,144 +1,113 @@
 use std::{
     sync::{
-        atomic::Ordering,
-        Arc, Mutex
+        atomic::{AtomicPtr, Ordering},
+        mpsc::{channel, Receiver, TryRecvError}
     },
-    thread::{Builder, JoinHandle, park, Thread},
+    thread::{Builder, JoinHandle, park},
 };
 use super::{
+    AtomicList, AtomicQueue,
     RUNNING,
-    AtomicOption
 };
 
-pub trait ThreadJob = Fn(ThreadPoolConnection) + Send + Sync + 'static;
+pub trait Thread<R> = FnOnce() -> R + Send + Sync + 'static;
+trait QueuedJob = FnOnce() + Send + Sync;
 
 #[derive(Clone)]
-pub struct ThreadPoolConnection {
-    id: usize,
-    parked: Arc<Mutex<bool>>,
-    handle: AtomicOption<&'static JoinHandle<()>>
+pub(crate) struct ThreadManager {
+    pool: AtomicList<JoinHandle<()>>,
+    park: AtomicList<AtomicPtr<JoinHandle<()>>>,
+    queue: AtomicQueue<Box<dyn QueuedJob>>
 }
 
-unsafe impl Send for ThreadPoolConnection {}
+unsafe impl Send for ThreadManager {}
 
-impl ThreadPoolConnection {
-    pub fn unpark_self(&self) {
-        if let Some(handle) = self.handle.try_unwrap() {
-            if let Ok(mut parked) = self.parked.lock() {
-                *parked = false;
-                handle.thread().unpark();
-            } 
-        }
-    }
-
-    pub fn park(&self) {
-        if let Ok(mut parked) = self.parked.lock() {
-            *parked = true;
-            park();
-        } 
-    }
-
-    pub fn running(&self) -> bool {
-        RUNNING.load(Ordering::Relaxed)
-    }
-}
-
-struct PoolThreadConnection {
-    id: usize,
-    parked: Arc<Mutex<bool>>,
-    handle: JoinHandle<()>
-}
-
-impl PoolThreadConnection {
-    fn is_parked(&self) -> Option<bool> {
-        if let Ok(parked) = self.parked.lock() {
-            Some(*parked)
-        } else {
-            None
-        }
-    }
-
-    fn thread(&self) -> &Thread {
-        self.handle.thread()
-    }
-
-    fn join(self) {
-        let name = (self.handle.thread().name().unwrap_or("Anonymous")).to_string();
-
-        if self.handle.join().is_err() {
-            println!("Unable to join with thread: \"{}\"!", name);
-        }
-    }
-}
-
-#[derive(Default)]
-pub struct ThreadPool{
-    threads: Vec<PoolThreadConnection>,
-}
-
-impl ThreadPool {
-    pub const fn new() -> Self {        
+impl ThreadManager {
+    pub fn new(queue_size:usize) -> Self {
         Self {
-            threads: Vec::new()
+            pool: AtomicList::new("Thread Pool", 0),
+            park: AtomicList::new("Thread Park", 0),
+            queue: AtomicQueue::new("Thread Job", queue_size)
         }
     }
 
-    pub fn init(&mut self, thread_count:usize) {
-        assert_ne!(self.threads.capacity(), 0, "Thread Pool is Already Initalized!"); 
-        self.threads = Vec::with_capacity(thread_count);
+    pub fn init(&self, size:usize) {
+        assert_eq!(self.pool.get_capacity(), 0, "Thread Pool is Already Initalized!");
+
+        self.pool.set_capacity(size);
+        self.park.set_capacity(size);
+
+        for id in 0..size {
+            let clone = self.clone();
+            let handle = Builder::new()
+                .name(id.to_string())
+                .spawn(move||{
+                    while RUNNING.load(Ordering::Relaxed) {
+                        match clone.next_job() {
+                            Some(job) => job(),
+                            None => clone.park(id)
+                        }
+                    }
+                })
+                .unwrap();
+
+            self.pool.push(handle);
+        }
     }
 
-    pub fn init_thread(&mut self, name:&str, thread:impl ThreadJob) {
-        let index = self.threads.len();
+    pub fn update_queue_capacity(&self, capcity:usize) {
+        self.queue.set_capacity(capcity);
+    }
 
-        if index >= self.threads.capacity() {
-            panic!("Thread Pool Full!")
+    fn next_job(&self) -> Option<Box<dyn QueuedJob>> {
+        self.queue.pop()
+    }
+
+    fn park(&self, id:usize) {
+        let ptr = self.pool.get(id).unwrap();
+        self.park.push(ptr);
+        park()
+    }
+
+    pub fn queue_thread<T:Send + 'static>(&self, func: impl Thread<T>) -> Job<T>{
+        let (sender, receiver) = channel::<T>();
+
+        self.queue.push(Box::new(move||sender.send(func()).unwrap()));
+
+        if let Some(handle) = self.park.pop() {
+            //SAFTEY: Handle has 'static lifetime
+            unsafe{ (**handle.as_ptr()).thread().unpark() }
         }
 
-        let builder = Builder::new().name(name.to_string());
-        let conn = ThreadPoolConnection{
-            id: index,
-            parked: Arc::new(Mutex::new(false)),
-            handle: AtomicOption::none()
-        };
-        let clone = conn.clone();
-
-        let handle = builder.spawn(move||thread(conn)).unwrap();
-        clone.handle.set(Some(unsafe {
-            &*(&handle as *const JoinHandle<()>)
-                as &JoinHandle<()>
-        }));
-
-        let handle = PoolThreadConnection {
-            id: index,
-            parked: clone.parked.clone(),
-            handle
-        };
-
-        self.threads.push(handle);
+        Job(receiver)
     }
 
-    pub fn unpark(&mut self, mut count:usize) {
-        for handle in &self.threads {
-            match handle.is_parked() {
-                Some(parked) => if parked {
-                    handle.thread().unpark();
-                    count -= 1;
-                },
-                //If Unsure, unpark anyway but don't count it.
-                None =>  handle.thread().unpark()
+    pub fn join_all(&self) {
+        while let Some(handle) = self.pool.pop() {
+            let name = handle.thread().name().unwrap_or("Anonymous").to_string();
+
+            if let Err(e) = handle.join() {
+                println!("Thread {name} paniced with error:\n{:?}", e)
             }
-
-            if count == 0 {
-                break;
-            }
-        }
-    }
-
-    pub fn join(mut self) {
-        while let Some(handle) = self.threads.pop() {
-            handle.thread().unpark();
-            handle.join()
         }
     }
 }
+
+pub struct Job<T:Send>(pub(crate) Receiver<T>);
+
+impl<T:Send> Future for Job<T> {
+    type Output = T;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        match self.0.try_recv() {
+            Ok(value) => std::task::Poll::Ready(value),
+            Err(e) => if e == TryRecvError::Empty {
+                cx.waker().wake_by_ref();
+                std::task::Poll::Pending
+            } else {
+                panic!("Actor disconected!")
+            }
+        }
+    }
+}
+
