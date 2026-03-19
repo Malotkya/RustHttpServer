edaa2e141e72882e68922fcfd33083e9acacce73
Alex M <ZimTKE@gmail.com>
2026-03-19 09:30:05 -0500

    Reworked thread job/proccess queue to help with async tasks when able.

--- async_lib/src/executor/thread.rs
+++ async_lib/src/executor/thread.rs
@@ -1,25 +1,42 @@
 use std::{
     sync::{
         atomic::{AtomicPtr, Ordering},
-        mpsc::{channel, Receiver, TryRecvError}
+        mpsc::{channel, Receiver, TryRecvError},
+        LazyLock
     },
-    thread::{Builder, JoinHandle, park},
+    thread::{Builder, JoinHandle},
 };
 use super::{
-    AtomicList, AtomicQueue,
-    RUNNING,
+    atomic::{AtomicList, AtomicQueue},
+    tasks::TASK_MANAGER,
+    RUNNING, DEFAULT_QUEUE_SIZE
 };
 
-pub trait Thread<R> = FnOnce() -> R + Send + Sync + 'static;
-trait QueuedJob = FnOnce() + Send + Sync;
+pub trait ThreadJob<R> = FnOnce() -> R + Send + Sync + 'static;
+pub trait ThreadProcess = ThreadJob<()>;
+
+pub(crate) static THREAD_MANAGER :LazyLock<ThreadManager> = LazyLock::new(||ThreadManager::new(DEFAULT_QUEUE_SIZE)); 
+
+pub(crate) fn thread_main(id:usize) {
+    while RUNNING.load(Ordering::Relaxed) {
+        if let Some(job) = THREAD_MANAGER.next_process() {
+            job();
+        } else if TASK_MANAGER.is_empty() {
+            THREAD_MANAGER.park(id);
+        } else {
+            TASK_MANAGER.run_next_task();
+        }
+    }
+}
 
 #[derive(Clone)]
 pub(crate) struct ThreadManager {
     pool: AtomicList<JoinHandle<()>>,
     park: AtomicList<AtomicPtr<JoinHandle<()>>>,
-    queue: AtomicQueue<Box<dyn QueuedJob>>
+    queue: AtomicQueue<Box<dyn ThreadProcess>>
 }
 
+// SAFETY: ThreadManager made up of sync safe components
 unsafe impl Send for ThreadManager {}
 
 impl ThreadManager {
@@ -37,20 +54,21 @@ impl ThreadManager {
         self.pool.set_capacity(size);
         self.park.set_capacity(size);
 
-        for id in 0..size {
-            let clone = self.clone();
-            let handle = Builder::new()
-                .name(id.to_string())
-                .spawn(move||{
-                    while RUNNING.load(Ordering::Relaxed) {
-                        match clone.next_job() {
-                            Some(job) => job(),
-                            None => clone.park(id)
-                        }
-                    }
-                })
+        let handle = Builder::new()
+                .name("0".to_string())
+                .spawn(TASK_MANAGER.thread_main(0))
                 .unwrap();
 
+        println!("Spawned 0");
+        self.pool.push(handle);
+
+        for id in 1..size {
+            let handle = Builder::new()
+                .name(id.to_string())
+                .spawn(move||thread_main(id))
+                .unwrap();
+
+            println!("Spawned {}", id);
             self.pool.push(handle);
         }
     }
@@ -59,26 +77,38 @@ impl ThreadManager {
         self.queue.set_capacity(capcity);
     }
 
-    fn next_job(&self) -> Option<Box<dyn QueuedJob>> {
+    fn next_process(&self) -> Option<Box<dyn ThreadProcess>> {
         self.queue.pop()
     }
 
-    fn park(&self, id:usize) {
-        let ptr = self.pool.get(id).unwrap();
-        self.park.push(ptr);
-        park()
+    pub fn park(&self, id:usize) {
+        if let Some(ptr) = self.pool.get(id) {
+            self.park.push(ptr);
+        }
+        std::thread::park();
     }
 
-    pub fn queue_thread<T:Send + 'static>(&self, func: impl Thread<T>) -> Job<T>{
-        let (sender, receiver) = channel::<T>();
+    pub fn unpark(&self, id:usize) {
+        if let Some(ptr) = self.pool.get(id) {
+            //SAFTEY: Handle has 'static lifetime
+            let thread = unsafe{ &*ptr.load(Ordering::Relaxed) }.thread();
+            thread.unpark();
+        }
+    }
 
-        self.queue.push(Box::new(move||sender.send(func()).unwrap()));
+    pub fn queue_process(&self, func: impl ThreadProcess) {
+        self.queue.push(Box::new(func));
 
         if let Some(handle) = self.park.pop() {
             //SAFTEY: Handle has 'static lifetime
-            unsafe{ (**handle.as_ptr()).thread().unpark() }
+            let thread = unsafe{ &*handle.load(Ordering::Relaxed) }.thread();
+            thread.unpark();
         }
+    }
 
+    pub fn queue_job<T:Send + 'static>(&self, func: impl ThreadJob<T>) -> Job<T>{
+        let (sender, receiver) = channel::<T>();
+        self.queue_process(move||sender.send(func()).unwrap());
         Job(receiver)
     }
 
@@ -87,7 +117,9 @@ impl ThreadManager {
             let name = handle.thread().name().unwrap_or("Anonymous").to_string();
 
             if let Err(e) = handle.join() {
-                println!("Thread {name} paniced with error:\n{:?}", e)
+                println!("Thread {name} paniced with error:\n{:?}", e);
+            } else {
+                println!("Thread {name} closed gracefully!");
             }
         }
     }
